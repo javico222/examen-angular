e2087b11c4fba31f12837d1d13e6445847219e08
javico222 <javico222@hotmail.com>
2023-04-23 14:16:25 -0500

    agregado routing

---
+++ src/app/components/pantalla-examen/pantalla-examen.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pantalla-examen',
+  templateUrl: './pantalla-examen.component.html',
+  styleUrls: ['./pantalla-examen.component.css']
+})
+export class PantallaExamenComponent {
+
+}

4f4dd3e4d202aa2a6a0d004946e2bf3ef16c8bdd
javico222 <javico222@hotmail.com>
2023-04-23 14:25:11 -0500

    agregado routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import { PantallaExamenComponent } from './components/pantalla-examen/pantalla-examen.component';
+
+const routes: Routes=[
+    { path: 'inicio',component: PantallaExamenComponent }
+    
+];
+
+@NgModule (
+    {
+        declarations: [],
+        imports: [
+            RouterModule.forRoot(routes)
+        ],
+        exports: [RouterModule]
+    }
+
+)
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonModule } from 'primeng/button';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PantallaExamenComponent } from './components/pantalla-examen/pantalla-examen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PantallaExamenComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
